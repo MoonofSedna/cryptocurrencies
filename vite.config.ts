04fc713a0bb1c2b12543ffba6927420a66fe4d90
Bethania Troconis <60520922+MoonofSedna@users.noreply.github.com>
2023-03-10 17:33:18 -0500

    unused dependecies removed and vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+/** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svgr(), react()],
+	plugins: [react()],
 });
